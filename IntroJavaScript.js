586f2ab364c58d1a0397fe83c7439390b1b6b5a0
AMANALIKHAN11 <amanalikhan5520@gmail.com>
2025-03-06 15:09:50 +0530

    Today I have learn setup of JS

---
+++ IntroJavaScript.js
@@ -0,0 +1,27 @@
+                // JAVA Script 
+
+
+// Java is high level and interprater programming  language used for  making Web Page 
+//  It is one of the core technologies of web development.
+
+
+                 // History Of JAVASCRIPT
+
+// 1995 Java Script created by Brendon Eich and was created for Netscap Navigator
+
+                 // Setup Development Enviroment & Running the Code
+
+//  We have a Two type of Setup Enviromet & Running The Code
+
+//  1 :- Download the Visual Code, After that Install the Extension of Javascript and we can Create a javascrip,HTML File
+        // Running Enviromnet :- when the our file is create with HTML file in VS code then After OPen the Browser we have a use
+        //                       and Click on right button after click the inspect option then after open will be Console Tab 
+        //                       and after the write code such as 
+        let printHello="Hello World";
+        console.log(printHello);
+
+//  2 :- Download the Node.js firstly node.js is runtime enviroment it allow javascript run to outside the browser
+//       after that install for your devices and chek the installation with this command "node --v" and after that create a file in VS Code
+//Running Enviroment :- when the file is created in vs code then After you write the code such as
+     let userName="Aman Ali Khan";
+     console.log(userName);  // And save this code after that open the terminal and write this command " node 'yourfilename'.js" then we can see the output
