8247bd44e0dec8444fdca6415a61481f01186cb5
AMANALIKHAN11 <amanalikhan5520@gmail.com>
2025-03-09 02:41:25 +0530

    Delete Namingconvention.js

--- Namingconvention.js
+++
@@ -1,9 +0,0 @@
-                                 //  Naming Convetion
-// Naming Convention means  in the javascript when we use variable, function, and class then we are follow best practice and convention to make the code easy nad read.
-
-let userName="I am Aman Ali Khan" // when we declare any variable then we are follow camelCase  But 
-const MOBILE="Apple"// when we declare const  then we will follow UPPER_CASE
-class SchoolOfVivekannda{
-    //when we creat a class file and class then we will follow PasscalCase
-}
-
